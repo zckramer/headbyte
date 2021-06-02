e5d24612c8cc0254c6169699f7b8934004c69e04
Zachary Kramer <zckramer83@gmail.com>
2021-06-02 16:14:42 -0400

    added thumbnails and nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import Logo1 from './images/Head byte Finale-01.png';
+import HB_Thumb from './images/Head byte Finale-03.png';
+import DT_Thumb from './images/dreadlogoonly2.png';
 
 const useStyles = createUseStyles({
     header: {
         position: 'absolute',
         width: '100vw',
-        height: '125px',
+        height: '120px',
         borderBottom: '1px solid black',
-        backgroundColor: 'gray'
+        backgroundColor: 'gray',
+        textAlign: 'center'
     },
     logo: {
         position: 'absolute',
         textAlign: 'center',
-        top: 5,
-        left: 5,
-        // margin: '1rem',
-        // padding: '.75rem',
+        top: 10,
+        left: 10,
         height: '100px',
         width: '100px',
-        border: '1px solid black',
-        borderRadius: '8px'
+        borderRadius: '8px',
+        backgroundColor: '#99aaff'
     },
     navbar: {
-        position: 'absolute',
+        position: 'fixed',
         right: 0,
-        top: 15,
+        top: 10,
         border: '1px solid black',
         borderRadius: '8px 0px 0px 8px',
         display: 'flex',
         flexDirection: 'row',
         alignContent: 'space-evenly',
-        padding: '.25rem'
+        padding: '.1rem',
+        backgroundColor: 'black',
+        maxHeight: '80px'
     },
     navbarItem: {
         margin: '1rem',
+        maxWidth: '60px',
+        maxHeight: '100%'
     }
-
 })
 
 const Header = () => {
@@ -45,9 +49,10 @@ const Header = () => {
     return (
         <div className={classes.header}>
             <img className={classes.logo} src={Logo1} alt='Headbyte Studios Logo'/>
+            <h1>HeadByte Studios</h1>
             <div className={classes.navbar}>
-                <div className={classes.navbarItem}>Item</div>
-                <div className={classes.navbarItem}>Item</div>
+                <img className={classes.navbarItem} src={DT_Thumb} alt='Nav Link to Dread Tides section'/>
+                <img className={classes.navbarItem} src={HB_Thumb} alt='Nav Link to HeadByte Home section'/>
                 <div className={classes.navbarItem}>Item</div>
                 <div className={classes.navbarItem}>Item</div>
             </div>
