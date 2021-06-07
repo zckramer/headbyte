833f865029650cd209a843c6296152c6ee424a6d
Zachary Kramer <zckramer83@gmail.com>
2021-06-07 13:19:45 -0400

    merge basic-header with master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import Logo1 from './images/Head byte Finale-01.png';
 import HB_Thumb from './images/Head byte Finale-03.png';
@@ -23,7 +23,7 @@ const useStyles = createUseStyles({
         borderRadius: '8px',
         backgroundColor: '#99aaff'
     },
-    navbar: {
+    navbarShow: {
         position: 'fixed',
         right: 0,
         top: 10,
@@ -34,29 +34,72 @@ const useStyles = createUseStyles({
         alignContent: 'space-evenly',
         padding: '.1rem',
         backgroundColor: 'black',
-        maxHeight: '80px'
+        maxHeight: '80px',
+    },
+    navbarHide: {
+        position: 'fixed',
+        right: -300,
+        top: 10,
+        border: '1px solid black',
+        borderRadius: '8px 0px 0px 8px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'space-evenly',
+        padding: '.1rem',
+        backgroundColor: 'black',
+        maxHeight: '80px',
     },
     navbarItem: {
         margin: '1rem',
         maxWidth: '60px',
         maxHeight: '100%'
-    }
-})
+    },
+    toggleButton: {
+        '&:hover $toggleHover': {
+            transform: '1.1rem',
+            color: 'blue'
+        },
+        position: 'absolute',
+        left: -15,
+        top: '25px',
+        backgroundColor: 'white',
+        border: '3px solid black',
+        borderRadius: '8px 0 0 8px',
+        cursor: 'pointer',
+        userSelect: 'none',
+        color: 'red',
+        transition: 'ease-in-out',
+    },
+    toggleHover: {
+        transform: '1.1rem',
+        color: 'blue'
 
+    }
+});
+
+// TODO: Abstract the Navigation out
 const Header = () => {
+    const [showNavbar, setShowNavbar] = useState(true);
+
+    function toggleNavbar () {
+        setShowNavbar(!showNavbar)
+        console.log('showNavBar = ', showNavbar);
+    };
+
     const classes = useStyles();
 
     return (
         <div className={classes.header}>
             <img className={classes.logo} src={Logo1} alt='Headbyte Studios Logo'/>
             <h1>HeadByte Studios</h1>
-            <div className={classes.navbar}>
-                <img className={classes.navbarItem} src={DT_Thumb} alt='Nav Link to Dread Tides section'/>
-                <img className={classes.navbarItem} src={HB_Thumb} alt='Nav Link to HeadByte Home section'/>
-                <div className={classes.navbarItem}>Item</div>
-                <div className={classes.navbarItem}>Item</div>
-            </div>
+                <div className={showNavbar ? classes.navbarShow : classes.navbarHide}>
+                    <div className={classes.toggleButton} onClick={()=>toggleNavbar()}>{showNavbar ? '>' : '<'}</div>
+                    <img className={classes.navbarItem} src={DT_Thumb} alt='Nav Link to Dread Tides section'/>
+                    <img className={classes.navbarItem} src={HB_Thumb} alt='Nav Link to HeadByte Home section'/>
+                    <div className={classes.navbarItem}>Item</div>
+                    <div className={classes.navbarItem}>Item</div>
+                </div>
         </div>
-    )
-}
+    );
+};
 export default Header;
