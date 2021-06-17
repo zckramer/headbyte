d968c6cba4217fbceca813c9abab2a7bdaaabcc6
Zachary Kramer <zckramer83@gmail.com>
2021-06-17 19:13:12 -0400

    added twitter widget and feed to DT content

--- src/components/MainDreadTides.jsx
+++ src/components/MainDreadTides.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
+import { Timeline } from 'react-twitter-widgets';
 
 const useStyles = createUseStyles({
     body: {
@@ -15,10 +16,21 @@ const useStyles = createUseStyles({
 
 const MainDreadTides = () => {
     const classes = useStyles();
-
+    const twitterFeed = '<a class="twitter-timeline" data-width="1200" data-height="900" href="https://twitter.com/DreadTides?ref_src=twsrc%5Etfw">Tweets by DreadTides</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
     return (
         <div className={classes.body}>
             <h1>Dread Tides</h1>
+            <Timeline
+                dataSource={{
+                    sourceType: 'profile',
+                    screenName: 'DreadTides'
+                }}
+                options={{
+                    height: '600',
+                    width: '800'
+                }}
+            />
+            {/* <div className='TwitterFeed' dangerouslySetInnerHTML={{__html: twitterFeed}}></div> */}
         </div>
     )
 }
